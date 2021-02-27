72732833db84c7acddce04811ff12689a6b7d5a5
MAnjaparidze <anjaparidzemamuka@gmail.com>
2021-02-27 16:17:44 +0400

    Refurbishing Styles

--- src/Components/Comments/CommentsSections.jsx
+++ src/Components/Comments/CommentsSections.jsx
@@ -5,8 +5,8 @@ import UserCommentModal from '../Comments/UserComment/UserCommentModal';
 export default function CommentsSections(props) {
     console.log("[PAGE RIGHT ", props);
     return (
-        <div className='card my-4 overflow-auto' style={{ padding: "0px 10px 0px 10px", maxHeight: "400px" }}>
-            <UserCommentModal />
+        <div className='card my-4 overflow-auto' style={{ position: 'relative', padding: "0px 10px 0px 10px", maxHeight: "500px" }}>
+            <UserCommentModal parentPost={props.comments[0].postId} />
             {props.comments.map(comment => {
                 return <CommentCard key={comment.id} comment={comment} />
             })}
--- src/Components/Comments/UserComment/UserCommentModal.jsx
+++ src/Components/Comments/UserComment/UserCommentModal.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 export default function UserCommentModal() {
     return (
-        <div className="card my-4">
+        <div className="card my-4" style={{position: "sticky", top: 0, left: 0, zIndex: 1020, width: "100%"}}>
           <h5 className="card-header">Leave a Comment:</h5>
           <div className="card-body">
             <form>
               <div className="form-group">
-                <textarea className="form-control" rows="3" style={{resize: "none", height: "100px"}}></textarea>
+                <textarea className="form-control" rows="3" style={{resize: "none", height: "80px"}}></textarea>
               </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
+              <button type="submit" className="btn btn-primary button-small">Submit</button>
             </form>
           </div>
         </div>
